e83bf58a7ad409a5f557b8802b0311505e740a79
Arleta <laililang@gmail.com>
2017-12-21 14:56:28 +0100

    game improved

--- src/components/game.vue
+++ src/components/game.vue
@@ -10,29 +10,31 @@
 <script>
 var propertyArray = [];
 export default {
-  data () {
-    return {
-        start: 'Start game',
-        cards: [
-            { id: 0, text: '<i class="demo-icon icon-user"></i>', checked: true, property: 'about', active: true },
-            { id: 1, text: '<i class="demo-icon icon-file-code"></i>', checked: true, property: 'projects', active: true },
-            { id: 2, text: '<i class="demo-icon icon-cog-alt"></i>', checked: true, property: 'skills', active: true },
-            { id: 3, text: '<i class="demo-icon icon-mail-1"></i>', checked: true, property: 'contact', active: true },
-            { id: 4, text: '<i class="demo-icon icon-github-circled"></i>', checked: true, property: 'github', active: true },
-            { id: 5, text: '<i class="demo-icon icon-linkedin"></i>', checked: true, property: 'linkedin', active: true },
-            { id: 6, text: '<i class="demo-icon icon-user"></i>', checked: true, property: 'about', active: true },
-            { id: 7, text: '<i class="demo-icon icon-file-code"></i>', checked: true, property: 'projects', active: true },
-            { id: 8, text: '<i class="demo-icon icon-cog-alt"></i>', checked: true, property: 'skills', active: true },
-            { id: 9, text: '<i class="demo-icon icon-mail-1"></i>', checked: true, property: 'contact', active: true },
-            { id: 10, text: '<i class="demo-icon icon-github-circled"></i>', checked: true, property: 'github', active: true },
-            { id: 11, text: '<i class="demo-icon icon-linkedin"></i>', checked: true, property: 'linkedin', active: true }
-        ]
-    }
-  },
-  methods: {
-      switchCard: function(card, cards) {
+    data () {
+        return {
+            start: 'Start game',
+            cards: [
+                { id: 0, text: '<i class="demo-icon icon-user"></i>', checked: true, property: 'about', active: true },
+                { id: 1, text: '<i class="demo-icon icon-file-code"></i>', checked: true, property: 'projects', active: true },
+                { id: 2, text: '<i class="demo-icon icon-cog-alt"></i>', checked: true, property: 'skills', active: true },
+                { id: 3, text: '<i class="demo-icon icon-mail-1"></i>', checked: true, property: 'contact', active: true },
+                { id: 4, text: '<i class="demo-icon icon-github-circled"></i>', checked: true, property: 'github', active: true },
+                { id: 5, text: '<i class="demo-icon icon-linkedin"></i>', checked: true, property: 'linkedin', active: true },
+                { id: 6, text: '<i class="demo-icon icon-user"></i>', checked: true, property: 'about', active: true },
+                { id: 7, text: '<i class="demo-icon icon-file-code"></i>', checked: true, property: 'projects', active: true },
+                { id: 8, text: '<i class="demo-icon icon-cog-alt"></i>', checked: true, property: 'skills', active: true },
+                { id: 9, text: '<i class="demo-icon icon-mail-1"></i>', checked: true, property: 'contact', active: true },
+                { id: 10, text: '<i class="demo-icon icon-github-circled"></i>', checked: true, property: 'github', active: true },
+                { id: 11, text: '<i class="demo-icon icon-linkedin"></i>', checked: true, property: 'linkedin', active: true }
+            ]
+        }   
+    },
+    methods: {
+        switchCard: function(card, cards) {
             var vm = this;
+            if(card.active === true){
             card.checked = !card.checked;
+            }
             function Array(propertyArray, cards) {
                 if(propertyArray.length < 2) {
                     propertyArray.push(card);
@@ -51,15 +53,16 @@ export default {
                 }
             }
             return console.log(Array(propertyArray));
-      }
-},
-  computed: {
+        },
+        turnCard: function()
+    },
+    computed: {
         randomList: function(){
-          return this.cards.sort(function() {
-              return 0 - Math.random()});
+            return this.cards.sort(function() {
+                return 0 - Math.random()});
+            }
         }
     }
-}
 </script>
 
 <style lang="scss">
