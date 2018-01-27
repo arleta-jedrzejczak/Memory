91832af227d02520fefcf1f39d6fbf1f908c05f7
Arleta <laililang@gmail.com>
2018-01-27 15:35:12 +0100

    publish

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -2,8 +2,8 @@
     <nav class="buttons">
         <div class="buttons__button buttons__button--back" v-on:click="moveComponent('Home')" v-show="back">Back to the game</div>
         <div class="buttons__button buttons__button--info" transition="expand" v-on:click="moveComponent('Instructions')" v-if="info">How to play?</div>
-        <a class="buttons__button--link" href="https://github.com/laililang" target="_blank" v-if="github"><div class="buttons__button buttons__button--github">Go to my GitHub</div></a>
-        <a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank" v-if="linked"><div class="buttons__button buttons__button--linkedin">Go to my LinkedIn</div></a>
+        <a class="buttons__button--link" href="https://github.com/laililang" target="_blank" v-if="github"><div class="buttons__button buttons__button--github">My GitHub</div></a>
+        <a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank" v-if="linked"><div class="buttons__button buttons__button--linkedin">My LinkedIn</div></a>
         <slot></slot>
     </nav>
 </template>
