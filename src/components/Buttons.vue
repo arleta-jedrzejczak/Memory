1b7acafe043acc8abc694aed2427b4c4fdab15b1
Arleta <laililang@gmail.com>
2018-01-27 16:04:27 +0100

    Revert "publish"
    This reverts commit e67ea3e7cceabbfc9eb394a8d00ef88c4f9341a9.

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,11 +1,11 @@
 <template>
-    <nav class="buttons">
+    <section class="buttons">
         <div class="buttons__button buttons__button--back" v-on:click="moveComponent('Home')" v-show="back">Back to the game</div>
         <div class="buttons__button buttons__button--info" transition="expand" v-on:click="moveComponent('Instructions')" v-if="info">How to play?</div>
         <a class="buttons__button--link" href="https://github.com/laililang" target="_blank" v-if="github"><div class="buttons__button buttons__button--github">Go to my GitHub</div></a>
-        <a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank" v-if="linked"><div class="buttons__button buttons__button--linkedin">Go to my LinkedIn</div></a>
+        <div class="buttons__button buttons__button--linkedin" v-if="linked"><a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank">Go to my LinkedIn</a></div>
         <slot></slot>
-    </nav>
+    </section>
 </template>
 
 <script>
