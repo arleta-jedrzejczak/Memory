298a8dfe11863f9d48402dca95deb7b9d53aa1f3
Arleta <laililang@gmail.com>
2018-01-27 02:08:05 +0100

    publish

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="buttons">
-        <button class="buttons__button buttons__button--back" v-on:click="moveComponent('Home')" v-show="back">Back to the game</button>
-        <button class="buttons__button buttons__button--info" transition="expand" v-on:click="moveComponent('Instructions')" v-if="info">How to play?</button>
-        <button class="buttons__button buttons__button--github" v-if="github"><a class="buttons__button--link" href="https://github.com/laililang" target="_blank">Go to my GitHub</a></button>
-        <button class="buttons__button buttons__button--linkedin" v-if="linked"><a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank">Go to my LinkedIn</a></button>
+        <div class="buttons__button buttons__button--back" v-on:click="moveComponent('Home')" v-show="back">Back to the game</div>
+        <div class="buttons__button buttons__button--info" transition="expand" v-on:click="moveComponent('Instructions')" v-if="info">How to play?</div>
+        <a class="buttons__button--link" href="https://github.com/laililang" target="_blank" v-if="github"><div class="buttons__button buttons__button--github">Go to my GitHub</div></a>
+        <div class="buttons__button buttons__button--linkedin" v-if="linked"><a class="buttons__button--link" href="https://pl.linkedin.com/in/arleta-j%C4%99drzejczak-167345147" target="_blank">Go to my LinkedIn</a></div>
         <slot></slot>
     </section>
 </template>
