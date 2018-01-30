e6e198ed33ee27fcf9f4877ef015ea4c4017f1ae
Arleta <laililang@gmail.com>
2018-01-30 13:48:17 +0100

    icon shadows added

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,9 +8,9 @@
         <span v-html="img"></span>
         <span v-html="imgsquare"></span>
         <article class="allabout__content">
-          <p>Hello, my name is <span class="allabout__content--bold">Arleta</span>. I'm web developer wannabe.
-          <p>I've been studying how to build websites for over a year. I'm interested in client and server sides in applications and besides explore front-end environment, also starting to learn PHP and MySQL.</p>
-          <p>I'm currently <span class="allabout__content--underline">looking for a first job or an internship as a junior front-end developer</span>.</p>
+          <p class="allabout__content--parag">Hello, my name is <span class="allabout__content--bold">Arleta</span>. I'm web developer wannabe.
+          <p class="allabout__content--parag">I've been studying how to build websites for over a year. I'm interested in client and server sides in applications and besides explore front-end environment, also starting to learn PHP and MySQL.</p>
+          <p class="allabout__content--parag">I'm currently <span class="allabout__content--bold">looking for a first job or an internship as a junior front-end developer</span>.</p>
         </article>
     </div>
   </section>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,42 +10,42 @@
             <ul class="my-skills">
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="HTML5">
-                        <span class="devicon-html5-plain"></span>
+                        <span class="my-skills__item--position devicon-html5-plain"></span>
                         <span class="my-skills__item--visuallyhidden">html 5 icon</span>
                     </div>
                     <p class="my-skills__description">HTML5</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="CSS3">
-                        <span class="devicon-css3-plain"></span>
+                        <span class="my-skills__item--position devicon-css3-plain"></span>
                         <span class="my-skills__item--visuallyhidden">css 3 icon</span>
                     </div>
                     <p class="my-skills__description">CSS3</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Sass/SCSS">
-                        <span class="devicon-sass-original"></span>
+                        <span class="my-skills__item--position devicon-sass-original"></span>
                         <span class="my-skills__item--visuallyhidden">sass icon</span>
                     </div>
                     <p class="my-skills__description">Sass/SCSS</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="RWD">
-                        <span class="my-skills__item--material material-icons">&#xE1B1;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE1B1;</span>
                         <span class="my-skills__item--visuallyhidden">mobile icon</span>
                     </div>
                     <p class="my-skills__description">RWD</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="JavaScript">
-                        <span class="devicon-javascript-plain"></span>
+                        <span class="my-skills__item--position devicon-javascript-plain"></span>
                         <span class="my-skills__item--visuallyhidden">javascript icon</span>
                     </div>
                     <p class="my-skills__description">JavaScript</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="VueJS 2">
-                        <span class="devicon-vuejs-plain"></span>
+                        <span class="my-skills__item--position devicon-vuejs-plain"></span>
                         <span class="my-skills__item--visuallyhidden">vue js icon</span>
                     </div>
                     <p class="my-skills__description">VueJS 2</p>
@@ -56,28 +56,28 @@
             <ul class="my-skills">
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Bootstrap">
-                        <span class="devicon-bootstrap-plain"></span>
+                        <span class="my-skills__item--position devicon-bootstrap-plain"></span>
                         <span class="my-skills__item--visuallyhidden">bootstrap icon</span>
                     </div>
                     <p class="my-skills__description">Bootstrap</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="jQuery">
-                        <span class="devicon-jquery-plain"></span>
+                        <span class="my-skills__item--position devicon-jquery-plain"></span>
                         <span class="my-skills__item--visuallyhidden">j query icon</span>
                     </div>
                     <p class="my-skills__description">jQuery</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="PHP">
-                        <span class="devicon-php-plain"></span>
+                        <span class="my-skills__item--position devicon-php-plain"></span>
                         <span class="my-skills__item--visuallyhidden">p h p icon</span>
                     </div>
                     <p class="my-skills__description">PHP</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="MySQL">
-                        <span class="devicon-mysql-plain"></span>
+                        <span class="my-skills__item--position devicon-mysql-plain"></span>
                         <span class="my-skills__item--visuallyhidden">my s q l icon</span>
                     </div>
                     <p class="my-skills__description">MySQL</p>
@@ -88,35 +88,35 @@
             <ul class="my-skills">
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Google Udacity Scholar 2017">
-                        <span class="devicon-google-plain"></span>
+                        <span class="my-skills__item--position devicon-google-plain"></span>
                         <span class="my-skills__item--visuallyhidden">google udacity scholar icon</span>
                     </div>
                     <p class="my-skills__description">Google Udacity Scholar 2017</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Learn IT, Girl 3rd edition">
-                        <span class="my-skills__item--material material-icons">&#xE1B0;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE1B0;</span>
                         <span class="my-skills__item--visuallyhidden">learn it girl third edition icon</span>
                     </div>
                     <p class="my-skills__description">Learn IT, Girl 3rd edition</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="VueJS 2 workshops - STXNext">
-                        <span class="devicon-vuejs-line"></span>
+                        <span class="my-skills__item--position devicon-vuejs-line"></span>
                         <span class="my-skills__item--visuallyhidden">VueJS 2 workshops icon</span>
                     </div>
                     <p class="my-skills__description">VueJS 2 workshops - STXNext</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="SQL workshops - Accenture">
-                        <span class="demo-icon icon-database"></span>
+                        <span class="my-skills__item--position demo-icon icon-database"></span>
                         <span class="my-skills__item--visuallyhidden">SQL workshops icon</span>
                     </div>
                     <p class="my-skills__description">SQL workshops - Accenture</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="EITC Photoshop certificate">
-                        <span class="devicon-photoshop-line"></span>
+                        <span class="my-skills__item--position devicon-photoshop-line"></span>
                         <span class="my-skills__item--visuallyhidden">photoshop certificate icon</span>
                     </div>
                     <p class="my-skills__description">EITC Photoshop certificate</p>
@@ -127,14 +127,14 @@
             <ul class="my-skills">
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Photoshop">
-                        <span class="devicon-photoshop-plain"></span>
+                        <span class="my-skills__item--position devicon-photoshop-plain"></span>
                         <span class="my-skills__item--visuallyhidden">photoshop icon</span>
                     </div>
                     <p class="my-skills__description">Photoshop</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="GIT">
-                        <span class="devicon-git-plain"></span>
+                        <span class="my-skills__item--position devicon-git-plain"></span>
                         <span class="my-skills__item--visuallyhidden">git icon</span>
                     </div>
                     <p class="my-skills__description">GIT</p>
@@ -145,28 +145,28 @@
             <ul class="my-skills">
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Communiaction">
-                        <span class="my-skills__item--material material-icons">&#xE023;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE023;</span>
                         <span class="my-skills__item--visuallyhidden">communication icon</span>
                     </div>
                     <p class="my-skills__description">Communication</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Teamwork">
-                        <span class="my-skills__item--material material-icons">&#xE7F0;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE7F0;</span>
                         <span class="my-skills__item--visuallyhidden">teamwork icon</span>
                     </div>
                     <p class="my-skills__description">Teamwork</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Positive attitude">
-                        <span class="my-skills__item--material material-icons">&#xE7F2;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE7F2;</span>
                         <span class="my-skills__item--visuallyhidden">positive attitude icon</span>
                     </div>
                     <p class="my-skills__description">Positive attitude</p>
                 </li>
                 <li class="my-skills__item">
                     <div class="my-skills__item--devicon" title="Responsibility">
-                        <span class="my-skills__item--material material-icons">&#xE8E8;</span>
+                        <span class="my-skills__item--position my-skills__item--material material-icons">&#xE8E8;</span>
                         <span class="my-skills__item--visuallyhidden">responsibility icon</span>
                     </div>
                     <p class="my-skills__description">Responsibility</p>
